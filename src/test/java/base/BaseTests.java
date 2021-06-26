c2fdb442d1303a47af976d626853b598e8d37d22
mehmetsahan58 <67297513+mehmetsahan58@users.noreply.github.com>
2021-06-26 15:51:58 -0500

    Base Tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -16,9 +20,17 @@ public class BaseTests {
 
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.manage().window().maximize();
-        driver.manage().window().fullscreen();
-        driver.manage().window().setSize(new Dimension(300, 300));
+//        driver.manage().window().maximize();
+//        driver.manage().window().fullscreen();
+//        driver.manage().window().setSize(new Dimension(300, 300));
+//
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println ("The number of websites: "+links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
+
 
         System.out.println ("The name of the website:  " + driver.getTitle());
 
